2088e60c480653788144bec4ed73452743234292
Bunescu <ion140290@gmail.com>
2023-01-11 11:07:03 +0200

    test deploy on AWS

--- src/Components/SectionTitle.js
+++ src/Components/SectionTitle.js
@@ -3,7 +3,7 @@ import "../styles/SectionTitle.css"
 export function SectionTitle() {
     return (
         <div className="section-title">
-            <h2>Resume</h2>
+            <h2>Resume (test deploy on AWS)</h2>
             <p>Check My Resume</p>
         </div>
     )
